b765438e03e2e6a5fab415a91c77715724d7e694
jellemannaerts <jelle.mannaerts@gmail.com>
2017-04-14 14:15:50 +0800

    Angular model classes added.

---
+++ src/app/model/DetailLayout.ts
@@ -0,0 +1,36 @@
+import {Layout} from "./Layout";
+
+export class DetailLayout extends Layout {
+  printBtnText: string;
+  printBtnColor: string;
+  printBtnImage: string;
+  printBtnBorderColor: string;
+  printBtnBorderWidth: number;
+  printBtnPressedColor: string;
+  printBtnPressedImage: string;
+  printBtnPressedBorderColor: string;
+  shareBtnText: string;
+  shareBtnColor: string;
+  shareBtnImage: string;
+  shareBtnBorderColor: string;
+  shareBtnBorderWidth: number;
+  shareBtnPressedColor: string;
+  shareBtnPressedImage: string;
+  shareBtnPressedBorderColor: string;
+  backBtnText: string;
+  backBtnColor: string;
+  backBtnImage: string;
+  backBtnBorderColor: string;
+  backBtnBorderWidth: number;
+  backBtnPressedColor: string;
+  backBtnPressedImage: string;
+  backBtnPressedBorderColor: string;
+  finishBtnText: string;
+  finishBtnColor: string;
+  finishBtnImage: string;
+  finishBtnBorderColor: string;
+  finishBtnBorderWidth: number;
+  finishBtnPressedColor: string;
+  finishBtnPressedImage: string;
+  finishBtnPressedBorderColor: string;
+}
---
+++ src/app/model/OverviewLayout.ts
@@ -0,0 +1,15 @@
+import {Layout} from "./Layout";
+
+export class OverviewLayout extends Layout {
+  imageContainer: boolean;
+  imageContainerColor: string;
+  imageContainerBorderColor: string;
+  imageContainerBorderWidth: number;
+  selectionIcon: string;
+  selectionContainerColor: string;
+  selectionContainerBorderColor: string;
+  selectionContainerBorderWidth: number;
+  selectBtnText: string;
+  navigationColor: string;
+  activeNavigationColor: string;
+}
