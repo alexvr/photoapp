a34645a61782f5001afe6e23fee8103ab1c10881
jellemannaerts <jelle.mannaerts@gmail.com>
2017-04-17 15:52:26 +0800

    configuration

--- src/app/configuration/configuration.component.ts
+++ src/app/configuration/configuration.component.ts
@@ -1,7 +1,8 @@
 import {Component} from "@angular/core";
 @Component({
   selector: 'configuration',
-  templateUrl: 'configuration.component.html'
+  templateUrl: 'configuration.component.html',
+  styleUrls: ['configuration.component.css']
 })
 
 export class ConfigurationComponent{
