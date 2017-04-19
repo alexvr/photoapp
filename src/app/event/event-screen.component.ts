2fbbf28ba83f2d63b2acb6a179754fa402af4a16
jellemannaerts <jelle.mannaerts@gmail.com>
2017-04-19 11:29:51 +0800

    event-screen component

--- src/app/event/event-screen.component.ts
+++ src/app/event/event-screen.component.ts
@@ -2,9 +2,15 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'event-screen',
-  templateUrl: 'event-screen.component.html'
+  templateUrl: 'event-screen.component.html',
+  styleUrls: ['event-screen.component.css']
 })
 
 export class EventScreenComponent{
-
+  config: Object = {
+    pagination: '.swiper-pagination',
+    slidesPerView: 3,
+    paginationClickable: true,
+    spaceBetween: 30
+  };
 }
