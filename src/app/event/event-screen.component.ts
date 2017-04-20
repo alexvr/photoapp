e00c7c0a5fe9b5d4c2c80e0d8e98338f647f8804
jellemannaerts <jelle.mannaerts@gmail.com>
2017-04-20 09:33:42 +0800

    printer-select in media settings

--- src/app/event/event-screen.component.ts
+++ src/app/event/event-screen.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {Component, ViewChild, ElementRef} from "@angular/core";
 
 @Component({
   selector: 'event-screen',
@@ -6,11 +6,43 @@ import {Component} from "@angular/core";
   styleUrls: ['event-screen.component.css']
 })
 
-export class EventScreenComponent{
-  config: Object = {
+export class EventScreenComponent {
+  private config: Object = {
     pagination: '.swiper-pagination',
     slidesPerView: 3,
     paginationClickable: true,
     spaceBetween: 30
   };
+
+  constructor() {
+  }
+
+  /* Show the menu on 3 clicks in left corner and 1 in the right corner. */
+  private exitCounter = 0;
+  private timer;
+  private visibleAnimate: boolean = false;
+  private visible: boolean = false;
+
+  showMenuLeftBtn() {
+    this.exitCounter++;
+    console.log(this.exitCounter);
+    clearTimeout(this.timer);
+    this.timer = setTimeout(() => this.exitCounter = 0, 3000);
+    console.log(this.exitCounter);
+  }
+
+  showMenuRightBtn() {
+    if (this.exitCounter >= 3) {
+      console.log("open menu!!");
+      this.visibleAnimate = true;
+      this.visible = true;
+      console.log(this.visibleAnimate + ' - ' + this.visible);
+    }
+  }
+
+  closeMenu(){
+    this.visibleAnimate = false;
+    this.visible = false;
+    console.log(this.visibleAnimate + ' - ' + this.visible);
+  }
 }
