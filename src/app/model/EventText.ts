41af9b0c8fdedcf8bd160b07ed6070daf5067c3c
stijnergeerts <KdG1p7PN>
2017-05-29 10:49:49 +0800

    Share page configuration

--- src/app/model/EventText.ts
+++ src/app/model/EventText.ts
@@ -2,10 +2,17 @@
  * Created by stijnergeerts on 15/05/17.
  */
 export class EventText {
+  eventName: string;
+  logo: string;
+  tabTitle: string;
   textUpper: string;
+  textAbovePhoto: string;
   textUnderPhoto: string;
   textBottom: string;
   textColor: string;
   backgroundColor: string;
+  backgroundImage: string;
+  backgroundImageToggle: boolean;
   cloudinaryDirectory: string;
+  qrCodeImage: string;
 }
